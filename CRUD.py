4c0f8b9bb663fb54ab71121b1ff221869d3aa604
Eric Felix <ericfelixfelixpinto@gmail.com>
2025-10-31 18:38:33 -0300

    crud

--- CRUD.py
+++ CRUD.py
@@ -1,30 +1,42 @@
-from interfaceUsuario import imprimeUsuario, atualizaDados
-from OperacaoMatematica import menuAluno, menuProfessor
 
 
-def create(dadosAlunos, dadosProfessor, escolha):
+def create(dadosAlunos, dadosProfessor, escolha, variavel1, variavel2, variavel3, variavel4, variavel5, variavel6, variavel7):
     if escolha == 1:
-        dadosAlunos = menuAluno(dadosAlunos)
+        dadosAlunos[variavel1] = {
+        "idade" : variavel2,
+        "peso" : variavel3,
+        "altura" : variavel4,
+        "sexo" : variavel5,
+        "imc" : variavel6,
+        "plano" : variavel7
+        }
+
     else:
-        dadosProfessor = menuProfessor(dadosProfessor)
+        dadosProfessor[variavel1] = {
+            "cpf" : variavel2,
+            "atuacao" : variavel3,
+            "capacitacao": variavel4,
+            "turno de trabalho": variavel5,
+            "carga horaria" : variavel6,
+            "salario" : variavel7 
+        }
     return dadosAlunos, dadosProfessor
         
       
 def read(dadosAlunos, dadosProfessor, usuario, nome):
     if (nome in dadosAlunos) or (nome in dadosProfessor):
-        imprimeUsuario(dadosAlunos, dadosProfessor, usuario, nome)
         return 1  
     else:
         return 0
 
         
        
-def update(dadosAlunos, dadosProfessor, usuario, nome):
-    if nome in dadosAlunos or nome in dadosProfessor:
-        dadosAlunos, dadosProfessor = atualizaDados(dadosAlunos, dadosProfessor, usuario, nome)
-        return dadosAlunos, dadosProfessor, 1
+def update(dadosAlunos, dadosProfessor, usuario, nome, campo, novoValor):
+    if usuario == "ALUNO":
+        dadosAlunos[nome][campo] = novoValor
     else:
-        return dadosAlunos, dadosProfessor, 0
+        dadosProfessor[nome][campo] = novoValor
+    return dadosAlunos, dadosProfessor
     
     
     
